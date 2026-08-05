6d23d92ac27781b5b79c980284e7fcef23d4d759
JAMILY <viktor19802710@gmail.com>
2026-08-05 21:04:25 +0300

    Start project

--- src/test/java/ui_tests/RegistrationTests.java
+++ src/test/java/ui_tests/RegistrationTests.java
@@ -1,4 +1,31 @@
 package ui_tests;
 
-public class RegistrationTests {
+import dto.UserLombok;
+import manager.AppManager;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+import java.util.Random;
+
+public class RegistrationTests extends AppManager {
+
+    @BeforeMethod
+    public void goToRegistrationLoginPage(){
+        new HomePage(getDriver()).clickBtnLogin();
+    }
+    @Test
+    public void registrationPositiveTest(){
+        int i = new Random().nextInt(1000);
+        UserLombok user = UserLombok.builder()
+                .username("vbfgtuuy34" + i + "@fgyuk.bn")
+                .password("Akyert23!")
+                .build();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginRegistrationForm(user);
+        loginPage.clickBtnRegistration();
+
+
+    }
 }
