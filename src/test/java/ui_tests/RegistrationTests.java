a123d7ace03c1f35a4053914de9aaa029f66bcc9
JAMILY <viktor19802710@gmail.com>
2026-08-09 21:06:20 +0300

    RegistrationTests

--- src/test/java/ui_tests/RegistrationTests.java
+++ src/test/java/ui_tests/RegistrationTests.java
@@ -2,18 +2,24 @@ package ui_tests;
 
 import dto.UserLombok;
 import manager.AppManager;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.ContactsPage;
 import pages.HomePage;
 import pages.LoginPage;
+import utils.UserFactory;
+import static utils.UserFactory.*;
 
 import java.util.Random;
 
 public class RegistrationTests extends AppManager {
+    LoginPage loginPage;
 
     @BeforeMethod
     public void goToRegistrationLoginPage(){
         new HomePage(getDriver()).clickBtnLogin();
+        loginPage = new LoginPage(getDriver());
     }
     @Test
     public void registrationPositiveTest(){
@@ -22,10 +28,40 @@ public class RegistrationTests extends AppManager {
                 .username("vbfgtuuy34" + i + "@fgyuk.bn")
                 .password("Akyert23!")
                 .build();
-        LoginPage loginPage = new LoginPage(getDriver());
+//        LoginPage loginPage = new LoginPage(getDriver());
         loginPage.typeLoginRegistrationForm(user);
         loginPage.clickBtnRegistration();
+        Assert.assertTrue(new ContactsPage(getDriver())
+                .validateTextInMessageNoContacts("No Contacts here!"));
+    }
+//    @Test
+//    public void testMethod(){
+//        new HomePage(getDriver()).method();
+//    }
+//    @Test
+//    public void testAjaxMethod() {
+//        new HomePage(getDriver()).ajaxMethod();
+//    }
+
+    @Test
+    public void registrationPositiveWithFakerTest(){
+        UserLombok user = positiveUser();
+        System.out.println(user);
+//        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginRegistrationForm(user);
+        loginPage.clickBtnRegistration();
+        Assert.assertTrue(new ContactsPage(getDriver())
+                .validateTextInMessageNoContacts("No Contacts here!"));
+
 
 
     }
+    @Test
+    public void registrationNegativeEmptyAllFieldsTest(){
+        loginPage.clickBtnRegistration();
+        Assert.assertTrue(loginPage.closeAlert()
+                .contains("Wrong email or password format"));
+    }
+
+
 }
